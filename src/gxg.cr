f366b54947e7ade03ea6cdf6e2b6d78b09151d52
G. Gibson <mistergibson@gxg.dev>
2023-03-30 07:51:09 -0700

    Bootstrap tweaks

--- src/gxg.cr
+++ src/gxg.cr
@@ -1,9 +1,11 @@
 require "anyolite"
 
-
+# ### Ruby core and external publications
 module GxG
-  VERSION = "0.1.0"
-
-  # TODO: Put your code here
+  VERSION = "0.0.1"
+  RUBY = Anyolite::RbInterpreter.new
+  module External
+  end
 end
-puts "hello world"
+Anyolite.wrap(GxG::RUBY, GxG::External)
+GxG::RUBY.load_script_from_file("Libraries/lib/bootstrap.rb")
